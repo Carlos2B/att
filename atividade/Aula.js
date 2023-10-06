a8722023ca80cbe3711ca2f571dd483e9f2c643a
CARLOS CESAR JUNIOR DOS SANTOS <carlos_cj_santos@estudante.sesisenai.org.br>
2023-10-06 07:48:22 -0300

    aaa

---
+++ atividade/Aula.js
@@ -0,0 +1,42 @@
+//function Valores(a, b){
+ //   var so = a + b
+//var m = a * b
+//var d = a/b
+//var su = a - b
+//return `A soma é, ${so},  a subtração é igual a ${su}, a multiplicação é ${m} e a divisão é ${d}.`
+//}
+//console.log(Valores(2, 4))
+
+//function Triangulos(a, b, c){
+  //  if(a==b==c){
+ //       retrun `Equilatero`
+   // }
+    //else if(a==b || a==c || c==b){
+     //   return`isoceles`
+    //}
+//else{
+ //   return`escaleno`
+//}
+//}
+//console.log(Triangulos(2,3,5))
+
+//function Parametros(a, b){
+  // var Resultado = a**b
+   //return`O resultado é ${Resultado}`
+//}
+//console.log(Parametros(2, 5))
+
+//function divisores(a, b){
+  //  var resultado = a/b
+    //var resto = a%b
+    //return `O resultado da divisão é ${resultado}, e o resto é ${resto}.`
+//}
+//console.log(divisores(5, 10))
+
+//function Moedareal(a){
+  //  var formatar = new Intl.NumberFormat('pt-BR', {style: 'currency', currency:'BRL'}).format(a);
+   // return `Arredondando o valor:0.30000000000000004, a quantidade adquirida é: ${formatar}`
+//}
+//console.log(Moedareal(0.30000000000000004))
+
+//function 
